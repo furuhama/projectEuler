49f6b7f00eb53694426d76e8d20a5932871cb1e7
furuhama <furuhama@users.noreply.github.com>
2017-12-28 10:00:51 +0900

    Add getDifferenceOfNaturalNum.cpp

---
+++ getDifferenceOfNaturalNum.cpp
@@ -0,0 +1,20 @@
+/* copyright furuhama 2017 */
+
+#include <iostream>
+
+int getSquareOfNaturalNum(int max) {
+    return ((1 + max) * max * (1 + max) * max / 4);
+}
+
+int main() {
+    int max = 100;
+    int sum = 0;
+    sum += getSquareOfNaturalNum(max);
+
+    for (int i = 1; i <= max; i++) {
+        sum -= i * i;
+    }
+
+    std::cout << "the answer is " << sum << std::endl;
+    return sum;
+}
