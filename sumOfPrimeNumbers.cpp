8171ad3519c2d4135308e0931cbaf990738fa1ac
furuhama <furuhama@users.noreply.github.com>
2017-12-31 22:07:20 +0900

    Add sumOfPrimeNumbers.cpp

---
+++ sumOfPrimeNumbers.cpp
@@ -0,0 +1,32 @@
+/* Copyright 2017 furuhama */
+
+#include <iostream>
+#include <cmath>
+#define N 2000000
+
+void Eratosthenes(int n, int *arr) {
+  for (int i = 0; i < n; i++) {
+    arr[i] = 1;
+  }
+  for (int i = 2; i < sqrt(n); i++) {
+    if (arr[i]) {
+      for (int j = 0; i * (j + 2) < n; j++) {
+        arr[i * (j + 2)] = 0;
+      }
+    }
+  }
+}
+
+int main() {
+    int arr[N];
+    Eratosthenes(N, arr);
+    // should use long type (instead of int)
+    long sumOfPrimes = 0;
+    for (int i = 2; i < N; i++) {
+        if (arr[i] == 1) {
+            sumOfPrimes = sumOfPrimes + i;
+        }
+    }
+    std::cout << "the answer is: " << sumOfPrimes << std::endl;
+    return 0;
+}
