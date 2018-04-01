17d2d07744382bcb349d1785c6a9caa6f7718613
furuhama <furuhama@users.noreply.github.com>
2018-04-01 10:19:38 +0900

    Solve no.23 non-abundant sums

--- archive/023_nonAbundantSums.rb
+++ archive/023_nonAbundantSums.rb
@@ -12,31 +12,24 @@ class Integer
       end
     end
 
-    if array.sum > self
-      return true
-    else
-      return false
-    end
+    return array.sum > self
   end
 
   def is_sum_of_abundant?
     ABUNDANT_NUMBERS.each do |abundant|
-      ABUNDANT_NUMBERS.include?(self - abundant)
+      break if abundant > ABUNDANT_NUMBERS.last / 2
+      return true if ABUNDANT_NUMBERS.include?(self - abundant)
     end
+
+    false
   end
 end
 
 if __FILE__ == $0
   # largest number that cannot be sum of two abundant number is 28123
   # smallest number that can be sum of two abundant number is 12
-  # ABUNDANT_NUMBERS = (12..28112).select { |i| i.is_abundant? }
-  # NOT_SUM_OF_TWO_ABUNDANT_NUMBERS = (24..28124).select { |i| !i.is_sum_of_abundant? }
-
-  ABUNDANT_NUMBERS = (12..30).select { |i| i.is_abundant? }
-  # NOT_SUM_OF_TWO_ABUNDANT_NUMBERS = (24..100).select { |i| !i.is_sum_of_abundant? }
-  (15..40).each do |i|
-    puts i if i.is_sum_of_abundant?
-  end
+  ABUNDANT_NUMBERS = (12..28112).select { |i| i.is_abundant? }
+  NOT_SUM_OF_TWO_ABUNDANT_NUMBERS = (24..28124).select { |i| !i.is_sum_of_abundant? }
 
   result = (1..23).to_a.sum + NOT_SUM_OF_TWO_ABUNDANT_NUMBERS.sum
   puts result
