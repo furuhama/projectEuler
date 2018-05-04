d09f12f643c6b7121cd2643d20b787b40acea0e9
furuhama <furuhama@users.noreply.github.com>
2018-05-05 00:56:47 +0900

    Fix range & Add other 3 fuctions for each direction & Solve

--- src/solver/number_spiral_diagonals.rs
+++ src/solver/number_spiral_diagonals.rs
@@ -1,11 +1,33 @@
 // problem no.28
 
 const SIZE: u64 = (1001 - 1) / 2;
+// const SIZE: u64 = 2;
 
-fn sum_center_right_up() -> u64 {
-    (1..SIZE).map(|e| e * e).fold(0, |acc, x| acc + x)
+fn sum_center_right_up(size: u64) -> u64 {
+    (0..size + 1)
+        .map(|e| 2 * e + 1)
+        .map(|e| e * e)
+        .fold(0, |acc, x| acc + x)
+}
+
+fn sum_right_down(size: u64) -> u64 {
+    (1..size + 1)
+        .map(|e| (2 * e - 1).pow(2) + 2 * e)
+        .fold(0, |acc, x| acc + x)
+}
+
+fn sum_left_down(size: u64) -> u64 {
+    (1..size + 1)
+        .map(|e| (2 * e).pow(2) + 1)
+        .fold(0, |acc, x| acc + x)
+}
+
+fn sum_left_up(size: u64) -> u64 {
+    (1..size + 1)
+        .map(|e| (2 * e).pow(2) + (2 * e) + 1)
+        .fold(0, |acc, x| acc + x)
 }
 
 pub fn solver() -> u64 {
-    sum_center_right_up()
+    sum_center_right_up(SIZE) + sum_right_down(SIZE) + sum_left_down(SIZE) + sum_left_up(SIZE)
 }
